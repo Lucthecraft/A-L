f0c37da83df3b3a643583a0ce72e4b07e9575187
Lucthecraft <l.uckyluc@hotmail.fr>
2017-06-16 21:17:43 +0200

    Ok

--- Template/Game1.cs
+++ Template/Game1.cs
@@ -21,8 +21,8 @@ namespace Template
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            graphics.PreferredBackBufferWidth = 1000;
-            graphics.PreferredBackBufferHeight = 800;
+            graphics.PreferredBackBufferWidth = 1280;
+            graphics.PreferredBackBufferHeight = 1024;
         }
 
 
@@ -32,7 +32,7 @@ namespace Template
             balTex = Content.Load<Texture2D>("ball");
             GOtex = Content.Load<Texture2D>("GO");
 
-            balXv = 15; balYv = 15; palv = 15;
+            balXv = 10; balYv = 10; palv = 15;
 
             balPos = new Vector2(GraphicsDevice.Viewport.Width / 2 - balTex.Width / 2, 0);
             palPos = new Vector2(GraphicsDevice.Viewport.Width / 2 - palTex.Width / 2, GraphicsDevice.Viewport.Height - palTex.Height * 1.5f);
@@ -63,20 +63,22 @@ namespace Template
                 balPos.Y += balYv;
                 if (Keyboard.GetState().IsKeyDown(Keys.Left)) { palPos.X -= palv; }
                 if (Keyboard.GetState().IsKeyDown(Keys.Right)) { palPos.X += palv; }
+
                 if (balPos.X <= 0) { balXv *= -1; }
                 if (balPos.X >= GraphicsDevice.Viewport.Width - balTex.Width) { balXv *= -1; }
                 if (balPos.Y <= 0) { balYv *= -1; }
                 if (balPos.Y >= GraphicsDevice.Viewport.Height - balTex.Height) { GO = true; }
-                if (balPos.Y >= palPos.Y - balTex.Width)
+                if (balPos.Y >= palPos.Y - balTex.Width & balPos.Y <= palPos.Y + palTex.Width / 2)
                 {
-                    if (balPos.X + balTex.Width >= palPos.X & balPos.X <= palPos.X + palTex.Width & balPos.Y >= palPos.Y + palTex.Width /2) { balYv *= -1; }
+                    if (balPos.X + balTex.Width >= palPos.X & balPos.X <= palPos.X + palTex.Width) { balYv *= -1; }
                 }
 
             }
 
             if(GO)
             {
-
+                balPos = new Vector2(GraphicsDevice.Viewport.Width / 2 - balTex.Width / 2, 0);
+                palPos = new Vector2(GraphicsDevice.Viewport.Width / 2 - palTex.Width / 2, GraphicsDevice.Viewport.Height - palTex.Height * 1.5f);
             }
 
             base.Update(gameTime);
@@ -95,6 +97,10 @@ namespace Template
             if(GO)
             {
                 spriteBatch.Draw(GOtex, GOpos, null);
+                if (Keyboard.GetState().IsKeyDown(Keys.R))
+                {
+                    GO = false;
+                }
             }
             spriteBatch.End();
 
