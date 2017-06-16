6093f1254a3c0ad6016d462c9f9dd6501535a7b2
Lucthecraft <l.uckyluc@hotmail.fr>
2017-06-16 20:20:05 +0200

    Ok

--- Template/Game1.cs
+++ Template/Game1.cs
@@ -9,13 +9,20 @@ namespace Template
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Texture2D palTex, balTex;
-        Vector2 palPos, balPos;
+        Texture2D palTex, balTex, GOtex;
+        Vector2 palPos, balPos, GOpos;
+
+        bool GO;
+        float balXv, balYv, palv;
+
+
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 1000;
+            graphics.PreferredBackBufferHeight = 800;
         }
 
 
@@ -23,6 +30,9 @@ namespace Template
         {
             palTex = Content.Load<Texture2D>("paddle");
             balTex = Content.Load<Texture2D>("ball");
+            GOtex = Content.Load<Texture2D>("GO");
+
+            balXv = 15; balYv = 15; palv = 15;
 
             balPos = new Vector2(GraphicsDevice.Viewport.Width / 2 - balTex.Width / 2, 0);
             palPos = new Vector2(GraphicsDevice.Viewport.Width / 2 - palTex.Width / 2, GraphicsDevice.Viewport.Height - palTex.Height * 1.5f);
@@ -47,6 +57,23 @@ namespace Template
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            if(!GO)
+            {
+                balPos.X += balXv;
+                balPos.Y += balYv;
+                palPos.X += palv;
+                if (balPos.X <= 0) { balXv *= -1; }
+                if (balPos.X >= GraphicsDevice.Viewport.Width - balTex.Width) { balXv *= -1; }
+                if (balPos.Y <= 0) { balYv *= -1; }
+                if (balPos.Y >= GraphicsDevice.Viewport.Height - balTex.Height) { GO = true; }
+
+            }
+
+            if(GO)
+            {
+
+            }
+
             base.Update(gameTime);
         }
 
@@ -55,8 +82,15 @@ namespace Template
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
-            spriteBatch.Draw(balTex, balPos, null);
-            spriteBatch.Draw(palTex, palPos, null);
+            if(!GO)
+            {
+                spriteBatch.Draw(balTex, balPos, null);
+                spriteBatch.Draw(palTex, palPos, null);
+            }
+            if(GO)
+            {
+                spriteBatch.Draw(GOtex, GOpos, null);
+            }
             spriteBatch.End();
 
             base.Draw(gameTime);
